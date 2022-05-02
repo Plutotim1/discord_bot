5eb7d4ccc0d4cc3e55ececa2351fb56bb03128f7
Plutotim1 <dimzollner@gmail.com>
2022-05-02 16:00:21 +0200

    added discord token

--- discord_bot.py
+++ discord_bot.py
@@ -22,4 +22,4 @@ class MyClient(discord.Client):
             await after.channel.send(f"{before.author} edited his message from '{str(before.content)}' to '{str(after.content)}'")
 
 client = MyClient()
-client.run("token")
+client.run("ODY5NjE4MjE3NTcwNjgwODYz.YQA1Ew.r3UnttxVcaogEzXp_rqRc0SyxKE")
